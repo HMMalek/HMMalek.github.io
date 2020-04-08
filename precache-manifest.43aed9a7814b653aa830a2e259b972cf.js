f276ce3706c837f69ea8ee040dc72f83c64c2139
HMMalek <hasna.m.malek@gmail.com>
2020-04-08 19:08:49 +0200

    Updates

---
+++ precache-manifest.43aed9a7814b653aa830a2e259b972cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2dd68b7cc66cc597d12c167654ebe5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7538a682bea5c85dbc4",
+    "url": "/static/css/main.6e63cf4d.chunk.css"
+  },
+  {
+    "revision": "38be37a957064efb1bdc",
+    "url": "/static/js/2.653ddddd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.653ddddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7538a682bea5c85dbc4",
+    "url": "/static/js/main.e7d681a5.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  }
+]);
